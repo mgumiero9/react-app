230d82cb7b68a7cf17d87f9ea090bc23ec56ed4c
Marcelo Gumiero <mgumiero9@gmail.com>
2018-08-28 23:11:37 -0300

    Simplest way to render. Showing how react works.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDom from 'react-dom';
 
 const element = <h1>Hello Word</h1>;
-console.log('​element', element);
-
+ReactDom.render(element, document.getElementById('root'));
